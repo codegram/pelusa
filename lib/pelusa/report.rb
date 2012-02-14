eed9c3bc98c1f226db344ff226b53c79de31b349
Josep M. Bach <josep.m.bach@gmail.com>
2012-02-14 13:12:44 +0100

    Documents report

--- lib/pelusa/report.rb
+++ lib/pelusa/report.rb
@@ -1,5 +1,12 @@
 module Pelusa
+  # Public: A Report is a wrapper that relates a class name with all its
+  # analyses for different lint checks.
+  #
   class Report
+    # Public: Initializes a new Report.
+    #
+    # class_name - The Symbol name of the class being analyzed.
+    # analyses   - An Array of Analysis objects.
     def initialize(class_name, analyses)
       @class_name = class_name
       @analyses   = analyses
