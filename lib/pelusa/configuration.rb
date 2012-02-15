9e1bf842b9faac7b1f571be843d97f30723e8bd7
Piotr Solnica <piotr.solnica@gmail.com>
2012-02-15 16:13:16 +0100

    Switch to tomdoc for Pelusa::Configuration

--- lib/pelusa/configuration.rb
+++ lib/pelusa/configuration.rb
@@ -1,58 +1,83 @@
 module Pelusa
-
-  # Class providing configuration options
+  # Public: Class providing configuration for the runner and lints
+  #
+  # Examples
+  #
+  #   configuration = Pelusa::Configuration.new('my_pelusa_config.yml')
+  #   configuration.present? # => true
   #
   class Configuration
     YAML_PATH = './.pelusa.yml'
 
-    # Initialize a configuration instance
+    # Public: Initializes a configuration instance
     #
-    # @param [String] optional path to the configuration file
-    #
-    # @api public
+    # yaml_path - optional path to the configuration file
     def initialize(yaml_path = YAML_PATH)
       if File.exist?(yaml_path)
         @_configuration = YAML.load_file(yaml_path).freeze
       end
     end
 
-    # @api public
+    # Public: Returns if a custom configuration is present
+    #
+    # Examples
+    #
+    #   Pelusa.configuration.present? # => true
+    #
     def present?
       not @_configuration.nil?
     end
 
-    # @api public
+    # Public: Returns custom configuration for the given lint
+    #
+    # Examples
+    #
+    #   Pelusa.configuration['LineRestriction'] # => {'limit' => 50}
+    #
+    # name - the name of the lint
     def [](name)
       for_lint(name)
     end
 
-    # @api public
+    # Public: Returns path to sources that should be analyzed
+    #
+    # Examples
+    #
+    #   Pelusa.configuration.sources # => lib/**/*.rb
+    #
     def sources
       @_configuration['sources']
     end
 
-    # @api public
+    # Public: Returns an Array of enabled lints
+    #
+    # Examples
+    #
+    #   Pelusa.configuration.enabled_lints # => [ Pelusa::Lint::DemeterLaw ]
+    #
     def enabled_lints
       (Lint.all - disabled_lints).uniq
     end
 
-    # @api public
-    def for_lint(name)
-      lints.fetch(name, {})
-    end
+    #######
+    private
+    #######
 
-  private
-
-    # @api private
+    # Private: Returns a Hash of configuration for lints
     def lints
       @_configuration.fetch('lints', {})
     end
 
-    # @api private
+    # Private: Returns an Array of lints disabled in the configuration
     def disabled_lints
       lints.select { |lint, conf| conf['enabled'] === false }.map do |lint,|
         Lint.const_get(lint)
       end
+    end
+
+    # Private: Public: Returns custom configuration for the given lint
+    def for_lint(name)
+      lints.fetch(name, {})
     end
 
   end # class Configuration
